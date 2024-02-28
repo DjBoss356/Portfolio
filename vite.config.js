7d2c8452dcd89787d0a98ec9f24b0311302f1155
DjBoss356 <155814344+DjBoss356@users.noreply.github.com>
2024-02-28 22:21:45 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],extends: ["eslint:all", "plugin:react/all"],
+  css:{
+    modules:{
+      localsConvention: "camelCase",
+    },
+  }
+})
